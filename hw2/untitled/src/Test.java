dfa8b04d564f6da25f0251119da9497ceed0671f
KucherenkoGeorgiy <georgiy.kucherenko@gmail.com>
2022-07-09 14:16:03 +0300

    testing encodings2.4

--- hw2/untitled/src/Test.java
+++ hw2/untitled/src/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("привет, Георгий22");
+        System.out.println("РїСЂРёРІРµС‚, Р“РµРѕСЂРіРёР№223");
     }
 }
