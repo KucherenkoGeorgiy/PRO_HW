a8d83b48cc4fdd946ac8d9a8410fa177fe051a11
KucherenkoGeorgiy <georgiy.kucherenko@gmail.com>
2022-07-13 14:45:28 +0300

    Adding home task 411

--- hw4/Runner.java
+++ hw4/Runner.java
@@ -2,9 +2,9 @@ package com.hillel.kucherenko.hw4;
 
 public class Runner {
     public static void main(String[] args) {
-        Car[] cars = {new Car ("Skoda", "Fabia", "2010", "blue", "6500", "123456"),
-                new Car ("Skoda", "Superb", "2010", "white", "6000", "516123"),
-                new Car ("Volkswagen", "Fabia", "2020", "red", "16500", "5555666")};
+        Car[] cars = {new Car("Skoda", "Fabia", "2010", "blue", "6500", "123456"),
+                new Car("Skoda", "Superb", "2010", "white", "6000", "516123"),
+                new Car("Volkswagen", "Fabia", "2020", "red", "16500", "5555666")};
         CarProcessor proc = new CarProcessor();
 
         proc.findAllByBrand(cars, "Skoda");
@@ -23,7 +23,7 @@ class Car {
     String price;
     String vinCode;
 
-     Car (String brand, String model, String yearOfIssue, String color, String price, String vinCode){
+    Car(String brand, String model, String yearOfIssue, String color, String price, String vinCode) {
         this.brand = brand;
         this.model = model;
         this.yearOfIssue = yearOfIssue;
@@ -34,20 +34,19 @@ class Car {
 }
 
 
-
 class CarProcessor {
-     void findAllByBrand (Car[] car, String brand){
+    void findAllByBrand(Car[] car, String brand) {
         for (int i = 0; i < car.length; i++) {
-            if (car[i].brand.equals(brand)){
+            if (car[i].brand.equals(brand)) {
                 printCarDetails(car[i]);
             }
         }
     }
 
-     void findAllByModelAndTimeOfUse(Car[] car, String model, String usageTime){
+    void findAllByModelAndTimeOfUse(Car[] car, String model, String usageTime) {
         for (int i = 0; i < car.length; i++) {
-            if ((((2022 - Integer.parseInt(car[i].yearOfIssue)) > Integer.parseInt(usageTime))
-                    && model.equals(car[i].model)) ){
+            if (((2022 - Integer.parseInt(car[i].yearOfIssue)) > Integer.parseInt(usageTime))
+                    && model.equals(car[i].model)) {
                 printCarDetails(car[i]);
             }
         }
@@ -55,15 +54,15 @@ class CarProcessor {
 
     }
 
-     void findAllByPriceAndYearOfIssue(Car[] car, String price, String yearOfIssue){
+    void findAllByPriceAndYearOfIssue(Car[] car, String price, String yearOfIssue) {
         for (int i = 0; i < car.length; i++) {
-            if ((Integer.parseInt(car[i].price) > Integer.parseInt(price)) && car[i].yearOfIssue.equals(yearOfIssue)){
+            if ((Integer.parseInt(car[i].price) > Integer.parseInt(price)) && car[i].yearOfIssue.equals(yearOfIssue)) {
                 printCarDetails(car[i]);
             }
         }
     }
 
-     void printCarDetails (Car car){
+    void printCarDetails(Car car) {
         System.out.println(car.brand + " " + car.model + " " + car.yearOfIssue
                 + " " + car.color + " " + car.price + " " + car.vinCode);
     }
